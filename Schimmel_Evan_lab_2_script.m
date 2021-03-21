a095551b61d62b4f24245282611b4bf80718d2c4
Evan Schimmel <evanrschimmel@gmail.com>
2021-03-21 15:09:26 -0400

    Part (a) working

--- Schimmel_Evan_lab_2_script.m
+++ Schimmel_Evan_lab_2_script.m
@@ -4,28 +4,28 @@ clc
 
 mp = 68.5e-3; %kg
 mw = 88e-3; %kg
-lp = 43.2e-3; %m
-dw = 5e-3; %m
-ds = 2.5e-3; %m
+lp = 43.2e-2; %m
+dw = 5e-2; %m
+ds = 2.5e-2; %m
 g = 9.81; %m/s^2
 
 tf = 2; %s
 maxstep = 0.01;
 tol = 1e-6;
 
-theta0 = -Pi/2; %rad
+theta0 = -pi/2; %rad
 omega0 = 0; %rad/s
 
 lpcg = (lp-ds)/2; %m
-lwcg = 20e-3; %m
+lwcg = 20e-2; %m
 
-J = (mp*lp^2)/12+mp*lpcg^2+(1/2)*mw*(dw/2)^2+mw*lwcg^2;
+J = ((mp*lp^2)/12)+mp*(lpcg^2)+(1/2)*mw*((dw/2)^2)+mw*(lwcg^2);
 
 k = g*(mp*lpcg+mw*lwcg);
 
 sim('Schimmel_Evan_lab_2_model')
 
 figure
-plot(t, v)
+plot(t, theta)
 xlabel('Time (s)')
 ylabel('Angular displacement (rad)')
